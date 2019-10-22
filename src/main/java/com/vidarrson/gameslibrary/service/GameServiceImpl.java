565009cc6b9c0150872ed97ebfe3f53249a7d8fb
VidarrSon <vitharrson@gmail.com>
2019-10-22 12:42:46 +0300

    Made class GameServiceImpl a @Service

--- src/main/java/com/vidarrson/gameslibrary/service/GameServiceImpl.java
+++ src/main/java/com/vidarrson/gameslibrary/service/GameServiceImpl.java
@@ -1,33 +1,47 @@
 package com.vidarrson.gameslibrary.service;
 
+import com.vidarrson.gameslibrary.dao.GameDao;
 import com.vidarrson.gameslibrary.model.Game;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class GameServiceImpl implements GameService{
+    private GameDao gameDao;
+
+    public void setGameDao(GameDao gameDao) {
+        this.gameDao = gameDao;
+    }
 
     @Override
+    @Transactional
     public void addGame(Game game) {
-
+        this.gameDao.addGame(game);
     }
 
     @Override
+    @Transactional
     public void updateGame(Game game) {
-
+        this.gameDao.updateGame(game);
     }
 
     @Override
+    @Transactional
     public void removeGame(int id) {
-
+        this.gameDao.removeGame(id);
     }
 
     @Override
+    @Transactional
     public Game getGameById(int id) {
-        return null;
+        return this.gameDao.getGameById(id);
     }
 
     @Override
+    @Transactional
     public List<Game> listGames() {
-        return null;
+        return this.gameDao.listGames();
     }
 }
