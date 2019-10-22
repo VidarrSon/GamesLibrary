732f421405f565139b773b6ae4927506c8cf79f0
VidarrSon <vitharrson@gmail.com>
2019-10-22 15:08:22 +0300

    Added @RequestMapping for different actions

--- src/main/java/com/vidarrson/gameslibrary/controller/GameController.java
+++ src/main/java/com/vidarrson/gameslibrary/controller/GameController.java
@@ -1,9 +1,15 @@
 package com.vidarrson.gameslibrary.controller;
 
+import com.vidarrson.gameslibrary.model.Game;
 import com.vidarrson.gameslibrary.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GameController {
@@ -15,4 +21,44 @@ public class GameController {
         this.gameService = gameService;
     }
 
+    @RequestMapping(value = "games", method = RequestMethod.GET)
+    public String listGames(Model model){
+        model.addAttribute("game", new Game());
+        model.addAttribute("listGames", this.gameService.listGames());
+
+        return "games";
+    }
+
+    @RequestMapping(value = "/games/add", method = RequestMethod.POST)
+    public String addGame(@ModelAttribute("game") Game game){
+        if(game.getId() == 0){
+            this.gameService.addGame(game);
+        } else {
+            this.gameService.updateGame(game);
+        }
+
+        return "redirect:/games";
+    }
+
+    @RequestMapping(value = "/remove/{id}")
+    public String removeGame(@PathVariable("id") int id){
+        this.gameService.removeGame(id);
+
+        return "redirect:/games";
+    }
+
+    @RequestMapping("edit/{id}")
+    public String editGame(@PathVariable("id") int id, Model model){
+        model.addAttribute("game", gameService.getGameById(id));
+        model.addAttribute("listGames", this.gameService.listGames());
+
+        return "games";
+    }
+
+    @RequestMapping("gamedata/{id}")
+    public String gameData(@PathVariable("id") int id, Model model){
+        model.addAttribute("game", this.gameService.getGameById(id));
+
+        return "gamedata";
+    }
 }
